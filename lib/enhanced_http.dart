927fd54a5141c4adb72c3972d612f011711e096e
Akalanka Perera <akalanka@parallax.lk>
2022-03-19 01:37:45 +0530

    Project initiated

---
+++ lib/enhanced_http.dart
@@ -0,0 +1,110 @@
+library enhanced_http;
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as p;
+import 'package:http_parser/http_parser.dart';
+
+class EnhancedHttp {
+
+  static late final String? _baseURL;
+  static late final Map<String,String>? _headers;
+  static late final Function? _errorTransformer;
+  static late final String? _defaultErrorMessage;
+
+  static void initialize({ required String baseURL, Map<String,String>? headers, Function? errorTransformer, String? defaultErrorMessage }) async {
+    _baseURL = baseURL;
+    _headers = headers ?? {};
+    _errorTransformer = errorTransformer;
+    _defaultErrorMessage = defaultErrorMessage;
+  }
+
+  static Future<dynamic> get({ required String path, required int successStatusCode, dynamic onSuccess }) async {
+    final url = Uri.parse('$_baseURL$path');
+    return await apiCall(() async {
+      return await http.get(
+        url,
+        headers: _headers,
+      );
+    }, successStatusCode, onSuccess);
+  }
+
+  static Future<dynamic> post({required String path, required int successStatusCode, dynamic payload, dynamic onSuccess, bool formData = false, dynamic file }) async {
+    final url = Uri.parse('$_baseURL$path');
+    return await apiCall(() async {
+      if (formData) {
+        return await multipartRequest('POST', url, payload, file);
+      } else {
+        return await http.post(
+          url,
+          headers:  _headers,
+          body: payload == null ? {} : jsonEncode(payload),
+        );
+      }
+    }, successStatusCode, onSuccess);
+  }
+
+  static Future<dynamic> put({ required String path, required int successStatusCode, dynamic payload, dynamic onSuccess, bool formData = false, dynamic file }) async {
+    final url = Uri.parse('$_baseURL$path');
+    return await apiCall(() async {
+      if (formData) {
+        return await multipartRequest('PUT', url, payload, file);
+      } else {
+        return await http.put(
+          url,
+          headers: _headers,
+          body: payload == null ? {} : jsonEncode(payload),
+        );
+      }
+    }, successStatusCode, onSuccess);
+  }
+
+  static Future<dynamic> delete({required String path, required int successStatusCode, dynamic onSuccess }) async {
+    final url = Uri.parse('$_baseURL$path');
+    return await apiCall(() async {
+      return await http.delete(
+        url,
+        headers: _headers,
+      );
+    }, successStatusCode, onSuccess);
+  }
+
+  static Future<dynamic> apiCall(Function httpRequest, int successStatusCode, dynamic onSuccess ) async {
+    try {
+      final response = await httpRequest();
+      Map<String, dynamic> responseJson = json.decode(response.body);
+      if (response.statusCode == successStatusCode) {
+        if (onSuccess != null) {
+          return onSuccess();
+        } else {
+          return responseJson;
+        }
+      } else {
+        print(response.statusCode);
+        if(_errorTransformer != null) return _errorTransformer!(responseJson);
+        return responseJson;
+      }
+    } catch (e) {
+      if(_defaultErrorMessage != null) return _defaultErrorMessage;
+      return e;
+    }
+  }
+
+  static multipartRequest(requestType, url, payload, file) async {
+    final request = http.MultipartRequest(requestType, url);
+    dynamic headers = _headers;
+    headers["Content-Type"] = "multipart/form-data";
+    request.headers.addAll(headers);
+    request.fields.addAll(Map<String, String>.from(payload));
+    if (file != null) {
+      request.files.add(await http.MultipartFile.fromPath(
+        'file',
+        file.path,
+        contentType: new MediaType("image", p.extension(file.path)),
+      ));
+    }
+    dynamic streamedResponse = await request.send();
+    return await http.Response.fromStream(streamedResponse);
+  }
+
+}
