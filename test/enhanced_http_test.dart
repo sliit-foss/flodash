cad35ac8b440aa02fb773da79f66d491abdbb25c
Akalanka Perera <akalanka@parallax.lk>
2022-04-14 09:36:11 +0530

    Added testing workflow and unit tests (#13)
    * Added testing workflow and unit tests
    
    * Update tests.yml

--- test/enhanced_http_test.dart
+++ test/enhanced_http_test.dart
@@ -2,13 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:enhanced_http/enhanced_http.dart';
 
 void main() {
-  EnhancedHttp.initialize(baseURL: "https://dog.ceo/api");
-  test('Fetches a random image of a dog from an api', () async {
-    dynamic results = await EnhancedHttp.get(path: "/breeds/image/random", successStatusCode: 200);
-    print(results);
-  });
-  test('Fetches a list of dog breeds from an api', () async {
-    dynamic results = await EnhancedHttp.get(path: "/breeds/list/all", successStatusCode: 200);
-    print(results);
+  group('Fetch API', ()
+  {
+    EnhancedHttp.initialize(baseURL: "https://dog.ceo/api");
+    test('Fetches a random image of a dog from an api', () async {
+      dynamic results = await EnhancedHttp.get(path: "/breeds/image/random", successStatusCode: 200);
+      expect(results['status'], 'success');
+    });
+    test('Fetches a list of dog breeds from an api', () async {
+      dynamic results = await EnhancedHttp.get(path: "/breeds/list/all", successStatusCode: 200);
+      expect(results['status'], 'success');
+    });
   });
 }
