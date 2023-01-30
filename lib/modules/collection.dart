486bebd163d7adcdd302080b542fae4d3aed84d5
Akalanka <akalankaperera128@gmail.com>
2023-01-30 20:21:51 +0530

    Added collection countBy

--- lib/modules/collection.dart
+++ lib/modules/collection.dart
@@ -1,3 +1,16 @@
+Map<dynamic, dynamic> countBy(List list, dynamic iteratee) {
+  return reduce(list, (Map<dynamic, dynamic> result, dynamic value, int index) {
+    final key = (iteratee is Function
+            ? iteratee(value)
+            : value is Map && iteratee is String
+                ? value[iteratee]
+                : iteratee)
+        .toString();
+    result[key] = result[key] == null ? 1 : result[key] + 1;
+    return result;
+  }, accumulator: {});
+}
+
 dynamic reduce(List list, dynamic iteratee, {dynamic accumulator}) {
   if (accumulator == null) {
     accumulator = list[0];
--- test/modules/collection_test.dart
+++ test/modules/collection_test.dart
@@ -3,8 +3,13 @@ import 'package:flodash/flodash.dart' as flodash;
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
+  test('count-by', () {
+    expect(flodash.countBy([6.1, 4.2, 6.3], (num) => num.floor()),
+        equals({'4': 1, '6': 2}));
+  });
   test('reduce', () {
-      expect(flodash.reduce([1, 2], (sum, n, i) => sum + n, accumulator: 0), equals(3));
-      expect(flodash.reduce([1, 2], (sum, n, i) => sum + n), equals(3));
-    });
+    expect(flodash.reduce([1, 2], (sum, n, i) => sum + n, accumulator: 0),
+        equals(3));
+    expect(flodash.reduce([1, 2], (sum, n, i) => sum + n), equals(3));
+  });
 }
