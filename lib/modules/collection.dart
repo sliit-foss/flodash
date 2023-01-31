60bfdad4d7bae0347182eee1f3133cb8c96b7c68
Akalanka <akalankaperera128@gmail.com>
2023-01-31 09:23:43 +0530

    Added collection each, eachRight, forEach, forEachRight

--- lib/modules/collection.dart
+++ lib/modules/collection.dart
@@ -11,6 +11,30 @@ Map<dynamic, dynamic> countBy(List list, dynamic iteratee) {
   }, accumulator: {});
 }
 
+void _each(dynamic collection, Function iteratee, {bool reverse = false}) {
+  for (int i = reverse ? collection.length - 1 : 0;
+      reverse ? i >= 0 : i < collection.length;
+      reverse ? i-- : i++) {
+    if (collection is List || collection is Set) {
+      iteratee(collection.elementAt(i), i);
+    } else if (collection is Map) {
+      String key = collection.keys.elementAt(i);
+      iteratee(collection[key], key);
+    } else {
+      throw Exception('Collection type not supported');
+    }
+  }
+}
+
+void each(dynamic collection, Function iteratee) => _each(collection, iteratee);
+
+void eachRight(dynamic collection, Function iteratee) =>
+    _each(collection, iteratee, reverse: true);
+
+void forEach = each;
+
+void forEachRight = eachRight;
+
 dynamic reduce(List list, dynamic iteratee, {dynamic accumulator}) {
   if (accumulator == null) {
     accumulator = list[0];
--- test/modules/collection_test.dart
+++ test/modules/collection_test.dart
@@ -7,6 +7,40 @@ main() {
     expect(flodash.countBy([6.1, 4.2, 6.3], (num) => num.floor()),
         equals({'4': 1, '6': 2}));
   });
+  group('each', () {
+    test('list', () {
+      List items = [];
+      flodash.each([1, 2], (n, i) => items.add(n*2));
+      expect(items, equals([2, 4]));
+    });
+    test('set', () {
+      List items = [];
+      flodash.each({1, 2}, (n, i) => items.add(n*2));
+      expect(items, equals([2, 4]));
+    });
+    test('map', () {
+      List items = [];
+      flodash.each({'a': 1, 'b': 2}, (value, key) => items.addAll([key, value]));
+      expect(items, equals(['a', 1, 'b', 2]));
+    });
+  });
+  group('each-right', () {
+    test('list', () {
+      List items = [];
+      flodash.eachRight([1, 2], (n, i) => items.add(n*2));
+      expect(items, equals([4, 2]));
+    });
+    test('set', () {
+      List items = [];
+      flodash.eachRight({1, 2}, (n, i) => items.add(n*2));
+      expect(items, equals([4, 2]));
+    });
+    test('map', () {
+      List items = [];
+      flodash.eachRight({'a': 1, 'b': 2}, (value, key) => items.addAll([key, value]));
+      expect(items, equals(['b', 2, 'a', 1]));
+    });
+  });
   test('reduce', () {
     expect(flodash.reduce([1, 2], (sum, n, i) => sum + n, accumulator: 0),
         equals(3));
