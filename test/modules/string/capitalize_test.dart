de585c331a56d8dcfe92191cf54381717dd027e3
miyurugunarathna <miyuru@spadesapp.io>
2023-06-14 15:47:49 +0530

    Test(Capitalize): German characters

--- test/modules/string/capitalize_test.dart
+++ test/modules/string/capitalize_test.dart
@@ -3,24 +3,28 @@ import 'package:flodash/flodash.dart' as flodash;
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('With all caps', () {
+  test('with all caps', () {
     String str = flodash.capitalize('FRED');
     expect(str, 'Fred');
   });
-  test('With mixed case', () {
+  test('with mixed case', () {
     String str = flodash.capitalize('FrED');
     expect(str, 'Fred');
   });
-  test('Two words both capitalized', () {
+  test('two words both capitalized', () {
     String str = flodash.capitalize('Deja Vu');
     expect(str, 'Deja vu');
   });
-  test('With all lowercase', () {
+  test('with all lowercase', () {
     String str = flodash.capitalize('kitten');
     expect(str, 'Kitten');
   });
-  test('All caps without first letter', () {
+  test('all caps without first letter', () {
     String str = flodash.capitalize('aDIDAS');
     expect(str, 'Adidas');
+  });
+  test('with a german character', () {
+    String str = flodash.capitalize('ßtrasse');
+    expect(str, 'SStrasse');
   });
 }
