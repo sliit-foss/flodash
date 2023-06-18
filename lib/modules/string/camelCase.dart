9d3957e0dc4750cf98baef2f9056348a6fb82825
miyurugunarathna <miyuru@spadesapp.io>
2023-06-18 22:36:45 +0530

    Refactor(camelCase): add proper types for intellisense

--- lib/modules/string/camelCase.dart
+++ lib/modules/string/camelCase.dart
@@ -3,8 +3,10 @@ import 'capitalize.dart';
 
 import '_helpers/main.dart' as helpers;
 
-Function camelCase =
+Function _camelCase =
     helpers.createCompounder((String result, String word, int index) {
   word = word.toLowerCase();
   return result + (index > 0 ? capitalize(word) : word);
 });
+
+String camelCase(String string) => _camelCase(string);
