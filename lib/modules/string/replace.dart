2e4455e8b68ff270ba43d3322296291ab5eeb7f6
GIHAA <gihansudeeparandimal@gmail.com>
2023-06-23 01:58:23 +0530

    Refactor: bug fixes

--- lib/modules/string/replace.dart
+++ lib/modules/string/replace.dart
@@ -1,4 +1,12 @@
+/// Replaces occurrences of pattern in string with replacement.
+/// Returns the modified string.
 String replace(String string, dynamic pattern, dynamic replacement) {
   Pattern patternObj = pattern is RegExp ? pattern : RegExp(pattern.toString());
-  return string.replaceAll(patternObj, replacement.toString());
+  if (replacement is Function) {
+    return string.replaceAllMapped(patternObj, (Match match) {
+      return replacement(match);
+    });
+  } else {
+    return string.replaceAll(patternObj, replacement.toString());
+  }
 }
--- test/modules/string/replace_test.dart
+++ test/modules/string/replace_test.dart
@@ -3,31 +3,27 @@ import 'package:flodash/flodash.dart' as flodash;
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  
-    test('replace should replace all occurrences of pattern in the string', () {
-      String input = 'Hello, Hello, Hello!';
-      String expected = 'Hi, Hi, Hi!';
-      String actual = flodash.replace(input, 'Hello', 'Hi');
-      expect(actual, expected);
-    });
+  test('replace should replace all occurrences of pattern in the string', () {
+    String input = 'Hello, Hello, Hello!';
+    String expected = 'Hi, Hi, Hi!';
+    String actual = flodash.replace(input, 'Hello', 'Hi');
+    expect(actual, expected);
+  });
+  test('should replace pattern with an empty string', () {
+    String input = 'Hello, World!';
+    String pattern = 'o';
 
-    test('should replace pattern with an empty string', () {
-      String input = 'Hello, World!';
-      String pattern = 'o';
+    String result = flodash.replace(input, pattern, '');
 
-      String result = flodash.replace(input, pattern, '');
-
-      expect(result, equals('Hell, Wrld!'));
-    });
-
-    test('should replace pattern with a numeric value', () {
-      String input = 'Price: \$100';
-      String pattern = r'\$';
-      String replacement = '200';
-
-      String result = flodash.replace(input, pattern, replacement);
-
-      expect(result, equals('Price: 200100'));
-    });
-
+    expect(result, equals('Hell, Wrld!'));
+  });
+  test('should replace pattern using a function', () {
+    String input = 'Hello, John!';
+    dynamic pattern = RegExp(r'\b\w+\b');
+    dynamic replacement = (match) {
+      return match.group(0).toUpperCase();
+    };
+    String result = flodash.replace(input, pattern, replacement);
+    expect(result, equals('HELLO, JOHN!'));
+  });
 }
