11629b0322f3945c4f13640c4251040a1eb00509
GIHAA <gihansudeeparandimal@gmail.com>
2023-06-24 19:39:37 +0530

    Feat: added trimStart under the string manipulation

--- lib/modules/string/trim_end.dart
+++ lib/modules/string/trim_end.dart
@@ -1,9 +1,7 @@
 ///Removes trailing whitespace or specified characters from string.
 String trimEnd(String str, [String chars = ' ']) {
-  if (str.isEmpty) 
-    return '';
+  if (str.isEmpty) return '';
   int end = str.length - 1;
-  while (end >= 0 && chars.contains(str[end])) 
-    end--;
+  while (end >= 0 && chars.contains(str[end])) end--;
   return str.substring(0, end + 1);
 }
