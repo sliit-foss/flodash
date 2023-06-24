86916794690687fc39293ab05020e91178a13a0d
GIHAA <gihansudeeparandimal@gmail.com>
2023-06-24 19:39:37 +0530

    Feat: added trimStart under the string manipulation

---
+++ packages/flodash/lib/modules/string/trimStart.dart
@@ -0,0 +1,11 @@
+///Removes leading whitespace or specified characters from string.
+String trimStart(String string, [String chars = ' ']) {
+  if (string.isEmpty) 
+    return string;
+  int startIndex = 0;
+  if (chars.isNotEmpty) {
+    while (startIndex < string.length && chars.contains(string[startIndex])) 
+      startIndex++;
+  }
+  return string.substring(startIndex);
+}
