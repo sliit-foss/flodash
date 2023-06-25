6d7ac7bc00a332c53f4e391e9af44eb9e32a4c5a
GIHAA <gihansudeeparandimal@gmail.com>
2023-06-25 14:47:45 +0530

    Refactor: remove new lines

--- packages/flodash/lib/modules/string/trim_end.dart
+++ packages/flodash/lib/modules/string/trim_end.dart
@@ -1,13 +1,9 @@
 ///Removes trailing whitespace or specified characters from string.
 String trimEnd(String str, [String chars = ' ']) {
-  if (str.isEmpty) {
+  if (str.isEmpty) 
     return '';
-  }
-
   int end = str.length - 1;
-  while (end >= 0 && chars.contains(str[end])) {
+  while (end >= 0 && chars.contains(str[end])) 
     end--;
-  }
-
   return str.substring(0, end + 1);
 }
